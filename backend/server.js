770f454f690bf4bf5966940e5f6328277a6f32ca
Kenny Ware <kennyware34@gmail.com>
2022-06-03 22:58:41 -0500

    Prepping deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 8000;
@@ -12,6 +13,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/sneakers", require("./routes/sneakerRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../fly-america/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "fly-america", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production."));
+}
 
 app.use(errorHandler);
 
